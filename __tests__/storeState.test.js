902deb7cbd67e857ec7e6db481123ad81161d4bf
David Boedigheimer <stealth259@gmail.com>
2021-09-13 14:31:09 -0700

    Add addition test.

--- __tests__/storeState.test.js
+++ __tests__/storeState.test.js
@@ -1,12 +1,18 @@
 import { stateControl, changeState } from "./../src/plantState.js";
 
 describe('stateControl', () => {
-    const newState = stateControl;
+    let newState = stateControl;
 
     test('should show the empty states of the plants', () => {
-        var newWater = changeState("water")(0);
-        newState = stateControl(newWater)
-        expect(newState).toEqual(0);
+        let newWater = changeState("water")(0);
+        newState = stateControl(newWater);
+        expect(newState).toEqual({ water: 0 });
+    });
+
+    test('should show the states incrementing for the plants on button push', () => {
+        let goodWater = changeState("water")(5);
+        newState = stateControl(goodWater);
+        expect(newState).toEqual({ water: 5 });
     });
 });
 
