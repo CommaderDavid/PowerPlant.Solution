761aad82863078e11e825eb448945707fcb20c13
David Boedigheimer <stealth259@gmail.com>
2021-09-15 16:02:15 -0700

    Add test for plant selection.

--- __tests__/storeState.test.js
+++ __tests__/storeState.test.js
@@ -2,19 +2,27 @@ import { stateControl, changeState } from "./../src/plantState.js";
 import { plant } from "./../src/newPlants.js";
 
 describe('stateControl', () => {
-    let newState = stateControl;
 
     test('should show the empty states of the plants', () => {
         let newWater = changeState("water")(0);
-        newState = stateControl(newWater);
+        let newState = stateControl(newWater);
         expect(newState).toEqual({ water: 0 });
     });
 
     test('should show the states incrementing for the plants on button push', () => {
         let goodWater = changeState("water")(5);
-        newState = stateControl(goodWater);
+        let newState = stateControl(goodWater);
         expect(newState).toEqual({ water: 5 });
     });
+
+    test('should show how the new plant edits the stateControl', () => {
+        let newSoil = changeState("soil")(4)
+        let newPlant = plant(2);
+        let newState = stateControl(newSoil);
+        console.log(newPlant(newState.soil))
+        console.log(newState)
+        expect(newPlant(newState.soil)).toEqual(8)
+    })
 });
 
 describe('newPlant', () => {
