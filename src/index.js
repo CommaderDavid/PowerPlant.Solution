672347b36e607ab7c86936c0bbdbe7389f40401f
David Boedigheimer <stealth259@gmail.com>
2021-09-16 14:57:56 -0700

    Add final change to reset page.

--- src/index.js
+++ src/index.js
@@ -74,13 +74,4 @@ $(document).ready(function () {
     // This function doesn't actually do anything useful in this application - it just demonstrates how
     // we can "look" at the current state (which the DOM is holding anyway). However, students often do
     // need the ability to see the current state without changing it so it's included here for reference.
-
-    $('#show-state').click(function () {
-        // We just need to call stateControl() without arguments to see our current state.
-        $('.states').show();
-        const currentState = stateControl();
-        $('#soil-value').text(`${currentState.soil}`);
-        $('#water-value').text(`${currentState.water}`);
-        $('#light-value').text(`${currentState.light}`);
-    });
 });
